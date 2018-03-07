55df307e661747b22ab547ffd3a6428b86957918
Allan Custodio de Lima <allan@Mac-Mini-Allan.local>
2018-03-07 12:45:22 -0300

    Initial commit

---
+++ Test/app/src/main/java/com/alli/test/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.alli.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Pizza", "McDonalds")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val radomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[radomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+
+        }
+    }
+}
